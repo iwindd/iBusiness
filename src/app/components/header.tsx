937be71be55cbe2cc840979ecedd47116b8c81fc
iwind <134185860+iwindd@users.noreply.github.com>
2023-12-04 20:07:08 +0700

    styles

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,24 +1,57 @@
 import React from 'react'
 import { Divider, Typography } from '@mui/material';
 
-const Header = (props: {
-  title: string,
+interface Header {
   children?: React.ReactNode,
   className?: string
-}) => {
+}
+
+interface HeaderCustom extends Header{
+  header: React.ReactNode
+}
+
+interface HeaderCompact extends Header{
+  title: string,
+}
+
+export const Header = (props : HeaderCustom) => {
   return (
     <>
-      <header className='mb-2 grid grid-cols-2'>
+      <HeaderRoot>
+        <main>
+          {props.header}
+        </main>
+        <article className={props.className}>
+          {props.children}
+        </article>
+      </HeaderRoot>
+      <Divider />
+    </>
+  )
+}
+
+const HeaderCompact = (props: HeaderCompact) => {
+  return (
+    <>
+      <HeaderRoot>
         <main>
           <Typography variant='h4'>{props.title}</Typography>
         </main>
         <article className={props.className}>
           {props.children}
         </article>
-      </header>
+      </HeaderRoot>
       <Divider />
     </>
   )
 }
 
-export default Header
+export const HeaderRoot = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <header className='mb-2 grid grid-cols-2'>
+      {children}
+    </header>
+  )
+}
+
+export default HeaderCompact
