2fce635de6b2a741da21d7a496199f3260f54be7
iwind <134185860+iwindd@users.noreply.github.com>
2023-12-04 21:42:17 +0700

    toast -> snackbar

--- src/app/providers/InterfaceProvider.tsx
+++ src/app/providers/InterfaceProvider.tsx
@@ -1,24 +1,21 @@
 "use client";
 import { Backdrop, CircularProgress } from '@mui/material';
-import React, { createContext, useContext, ReactNode, useEffect } from 'react';
+import React, { createContext, useContext, ReactNode } from 'react';
 import Dialog, { DialogProps as OriginalDialogProps } from '@mui/material/Dialog';
-
-interface ToastInterface {
-  useToast: (message: string, className: string) => void
-}
+import { SnackbarProvider } from 'notistack';
 
 interface BackdropInterface {
   useBackdrop: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 type theme = "light" | "dark";
-interface ThemeInterface{
+interface ThemeInterface {
   theme: theme,
   setTheme: React.Dispatch<React.SetStateAction<theme>>
 }
 
 type shopType = "retail" | "wholesale"
-interface ShopInterface{
+interface ShopInterface {
   shop: shopType,
   setShop: React.Dispatch<React.SetStateAction<shopType>>
 }
@@ -33,7 +30,7 @@ interface DialogInterface {
   useDialog: (Content: React.FC<DialogProps>, data: any, size?: OriginalDialogProps['maxWidth']) => DialogProps
 }
 
-interface InterfaceData extends ToastInterface, BackdropInterface, DialogInterface, ThemeInterface, ShopInterface {};
+interface InterfaceData extends BackdropInterface, DialogInterface, ThemeInterface, ShopInterface { };
 
 const InterfaceContext = createContext<InterfaceData | undefined>(undefined);
 
@@ -55,19 +52,6 @@ export function InterfaceProvider({ children }: {
   const [shop, setShop] = React.useState<shopType>("retail");
   const [DialogContent, setDialogContent] = React.useState<null | JSX.Element>(null);
   const [maxWidth, setMaxWidth] = React.useState<OriginalDialogProps['maxWidth']>('sm');
-
-  const [toasts, setToasts] = React.useState<{
-    msg: string,
-    className: string,
-    index: number
-  }[]>([])
-
-  const useToast = (msg: string, className: string) => {
-    ToastIndex++;
-    const saveIndex = ToastIndex
-    setToasts(toasts => [...toasts, { msg, className, index: saveIndex }])
-    setTimeout(() => setToasts(toasts => toasts.filter(t => t.index != saveIndex)), 5000)
-  }
 
   const useDialog = (
     Content: React.FC<DialogProps>,
@@ -93,47 +77,46 @@ export function InterfaceProvider({ children }: {
     return dialogProps
   }
 
-  return <InterfaceContext.Provider
-    value={
-      {
-        useToast: useToast,
-        useBackdrop: setBackdrop,
-        useDialog: useDialog,
-        theme: theme,
-        setTheme: setTheme,
-        shop: shop,
-        setShop: setShop
+  return (
+    <InterfaceContext.Provider
+      value={
+        {
+          useBackdrop: setBackdrop,
+          useDialog: useDialog,
+          theme: theme,
+          setTheme: setTheme,
+          shop: shop,
+          setShop: setShop
+        }
       }
-    }
-  >
-    <div className="toast toast-end">
-{/*       {
-        toasts.map((t) => {
-          return (
-            <div className={t.className}>
-              <span>{t.msg}</span>
-            </div>
-          )
-        })
-      } */}
-    </div>
-    {children}
-    <Backdrop
-      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={isBackdrop}
     >
-      <CircularProgress color="inherit" />
-    </Backdrop>
-    <Dialog
-      open={isDialog}
-      onClose={() => setDialog(false)}
-      aria-labelledby="responsive-dialog-title"
-      fullWidth={true}
-      maxWidth={maxWidth}
-    >
-      {DialogContent}
-    </Dialog>
-  </InterfaceContext.Provider>;
+      <SnackbarProvider
+        maxSnack={5}
+        anchorOrigin={{
+          horizontal: "right",
+          vertical: "bottom"
+        }}
+      >
+        {children}
+      </SnackbarProvider>
+
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isBackdrop}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Dialog
+        open={isDialog}
+        onClose={() => setDialog(false)}
+        aria-labelledby="responsive-dialog-title"
+        fullWidth={true}
+        maxWidth={maxWidth}
+      >
+        {DialogContent}
+      </Dialog>
+    </InterfaceContext.Provider>
+  )
 }
 
 export default InterfaceContext;
