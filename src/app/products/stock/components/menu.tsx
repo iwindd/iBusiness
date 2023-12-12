1bb90b2ce1d2fc7c1b187d250d4f75190335fbfb
iwind <134185860+iwindd@users.noreply.github.com>
2023-12-12 21:05:36 +0700

    layout

--- src/app/products/stock/components/menu.tsx
+++ src/app/products/stock/components/menu.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import File from './menu/file'
 import Commit from './menu/commit'
+import Controller from './menu/controller'
 
 const Menu = () => {
   return (
-    <section className='space-x-2 flex'>
-      <File />
-      <Commit />
+    <section className='space-x-2 grid md:grid-cols-2 sm:grid-cols-1'>
+      <section className='flex'>
+        <File />
+        <Commit />
+      </section>
+      <section className='flex md:justify-end sm:justify-start'>
+        <Controller />
+      </section>
     </section>
   )
 }
---
+++ src/app/products/stock/components/menu/controller.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Controller = () => {
+  return (
+    <div >
+    </div>
+  )
+}
+
+export default Controller
