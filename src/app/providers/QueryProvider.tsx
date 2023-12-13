3a571a6eaf8012ed27b589d6a9a797f26e89b86b
iwind <134185860+iwindd@users.noreply.github.com>
2023-12-13 18:43:56 +0700

    caching query

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -1,11 +1,21 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export default function QueryProvider({ children }: {
   children: React.ReactNode
 }) {
   const [queryClient] = useState(() => new QueryClient());
+
+  useEffect(() => {
+    queryClient.setDefaultOptions({
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchInterval: 5 * 60 * 1000,
+      },
+    })
+  })
 
   return (
     <QueryClientProvider client={queryClient}>
