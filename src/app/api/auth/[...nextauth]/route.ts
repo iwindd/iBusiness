ad3403629b4aa46fa77c126f24f966f9470f0f4f
iwindd <freewinendlife@gmail.com>
2024-05-16 18:53:12 +0700

    fix auth api

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,12 @@
-import NextAuth from 'next-auth'
-import { authOptions } from '@/auth'
+import type { NextRequest } from "next/server"
+import NextAuth from "next-auth"
+import { authOptions } from "@/auth"
+interface RouteHandlerContext {
+  params: { nextauth: string[] }
+}
 
-export const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+async function auth(req: NextRequest, context: RouteHandlerContext) {
+  return await NextAuth(req, context, authOptions)
+}
+
+export { auth as GET, auth as POST }
