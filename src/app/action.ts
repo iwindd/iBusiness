9ac3f9a1ef2fc8a7115d17d155828c9d905e7de2
iwindd <freewinendlife@gmail.com>
2024-05-16 17:28:04 +0700

    re structure

--- src/app/action.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-import { getServerSession } from '@/libs/session';
-
-export const getSessionAction = async () => {
-  const session = await getServerSession();
-  
-  return session
-}
