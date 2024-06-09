d8f4b99c586145a30b5fffc5dd9a8eb9910b0cc1
iwindd <freewinendlife@gmail.com>
2024-06-09 16:25:55 +0700

    chore : remove comments

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -9,20 +9,3 @@ export const prisma = global.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export * from "@prisma/client"
-
-/* 
-import { PrismaClient } from '@prisma/client'
-
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma */
