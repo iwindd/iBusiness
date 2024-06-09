a1f761a313e12cef052ae5fa1cfb4fe754c57cb4
iwindd <freewinendlife@gmail.com>
2024-06-09 16:04:45 +0700

    chore: global prisma

--- apps/hub/src/libs/prisma.ts
+++ apps/hub/src/libs/prisma.ts
@@ -1,15 +1,3 @@
-import { PrismaClient } from '@prisma/client'
+import { prisma } from "@repo/db";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma;
